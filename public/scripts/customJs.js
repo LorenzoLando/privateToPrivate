b99ce8ee16e6dafed341b5f35829f98c87e37274
Lorenzo Lando <lorenzo.lando85@gmail.com>
2019-09-24 13:02:37 +0000

    added front-end logic to forms display

--- public/scripts/customJs.js
+++ public/scripts/customJs.js
@@ -1,1 +1,17 @@
-alert("custom javascript is working");
+//changing the displayed search form
+//1- selecting all search forms buttons
+//2- selecting all search forms
+//3- when a user click on a button
+	//3.1- remove active class form all forms
+		//3.2- adding class active on the related form
+const formButtons = document.querySelectorAll(".form-button"); //1
+const forms = document.querySelectorAll(".formContainer form"); //2
+formButtons.forEach((formButton, index) => {
+    formButton.addEventListener('click', () => { //3
+        forms.forEach((form) => {
+            form.classList.remove("active"); //3.1
+        });
+        forms[index].classList.add("active");  //3.2    
+
+    });
+});
